44c2c11e0fb81f8eff5db34f6bcbcd75c7c370f0
mpls <mpls@posteo.net>
2022-11-20 15:24:05 -0600

    include js from template

--- build.rs
+++ build.rs
@@ -1,16 +1,34 @@
-use std::process::Command;
+use askama::Template;
+use std::{
+    fs::{self, File},
+    path::Path,
+    process::Command,
+};
 
 fn main() {
+    #[derive(Template)]
+    #[template(path = "index.html", escape = "none")]
+    struct IndexTemplate {
+        js: String,
+    }
+
+    // Generate wasm and js
     Command::new("wasm-bindgen")
         .arg("--target")
         .arg("no-modules")
         .arg("--no-typescript")
         .arg("--out-dir")
         .arg("www/scripts")
-        .arg(
-            "target/wasm32-unknown-u\
-nknown/debug/my_page.wasm",
-        )
+        .arg("target/wasm32-unknown-unknown/debug/my_page.wasm")
         .output()
         .unwrap();
+
+    // Grab js and store it in a template
+    let template_path = Path::new("www").join("scripts").join("my_page.js");
+    let js = fs::read_to_string(template_path).expect("we opened my_page.js");
+    let index_template = IndexTemplate { js };
+    let contents = index_template.render().unwrap();
+
+    let index_path = Path::new("www").join("index.html");
+    fs::write(index_path, contents).expect("wrote to index.html");
 }
